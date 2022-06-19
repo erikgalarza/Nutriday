1085e0e2b20cfe8b7119090f4a4c9efe591b8d4f
AlvaroSalazar25 <alvaro_wtf06@hotmail.com>
2022-06-19 12:43:55 -0500

    fix: dashboard paciente

--- resources/views/client/principal.blade.php
+++ resources/views/client/principal.blade.php
@@ -1,87 +1,84 @@
 @extends('client.dashboard')
 @section('contenido')
+    <div class="page-header mb-2">
+        <h3 class="page-title">
+            Inicio
+        </h3>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+            </ol>
+        </nav>
+    </div>
 
-<div class="page-header">
-    <h3 class="page-title">
-        Página de inicio
-    </h3>
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
-        </ol>
-    </nav>
-</div>
-
-<div class="card">
-            <div class="col-12 p-0">
-                <div class="card card-statistics">
-                  <div class="card-body">
+    <div class="card">
+        <div class="col-12 p-0">
+            <div class="card card-statistics">
+                <div class="card-body">
                     <div class="d-flex flex-column flex-md-row align-items-center justify-content-between">
-                        <div class="statistics-item text-center"  style="max-width: 120px" >
-                          <p>
-                            <i class="fa-regular fa-calendar-minus"></i>
-                        </p>
-                          <h2 class="" style="font-size:18px;text-transform:uppercase">Lunes</h2>
-                          <label class="badge badge-outline-success badge-pill mt-3">Ver detalles</label>
+                        <div class="statistics-item text-center" style="max-width: 120px">
+                            <p>
+                                <i class="fa-regular fa-calendar-minus"></i>
+                            </p>
+                            <h2 class="" style="font-size:18px;text-transform:uppercase">Lunes</h2>
+                            <label class="badge badge-outline-success badge-pill mt-3">Ver detalles</label>
                         </div>
-                        <div class="statistics-item text-center"  style="max-width: 120px" >
-                          <p>
-                            <i class="fa-regular fa-calendar-minus"></i>
-                        </p>
-                          <h2  class="" style="font-size:18px;text-transform:uppercase">Martes</h2>
-                          <label class="badge badge-outline-danger badge-pill mt-3">Ver detalles</label>
+                        <div class="statistics-item text-center" style="max-width: 120px">
+                            <p>
+                                <i class="fa-regular fa-calendar-minus"></i>
+                            </p>
+                            <h2 class="" style="font-size:18px;text-transform:uppercase">Martes</h2>
+                            <label class="badge badge-outline-danger badge-pill mt-3">Ver detalles</label>
                         </div>
-                        <div class="statistics-item text-center " style="max-width: 120px" >
-                          <p>
-                            <i class="fa-regular fa-calendar-minus"></i>
-                          </p>
-                          <h2  class="" style="font-size:18px;text-transform:uppercase">Miércoles</h2>
-                          <label class="badge badge-outline-success badge-pill mt-3">Ver detalles</label>
+                        <div class="statistics-item text-center " style="max-width: 120px">
+                            <p>
+                                <i class="fa-regular fa-calendar-minus"></i>
+                            </p>
+                            <h2 class="" style="font-size:18px;text-transform:uppercase">Miércoles</h2>
+                            <label class="badge badge-outline-success badge-pill mt-3">Ver detalles</label>
                         </div>
-                        <div class="statistics-item text-center"  style="max-width: 120px" >
-                          <p>
-                            <i class="fa-regular fa-calendar-minus"></i>
-                        </p>
-                          <h2  class="" style="font-size:18px;text-transform:uppercase">Jueves</h2>
-                          <label class="badge badge-outline-success badge-pill mt-3">Ver detalles</label>
+                        <div class="statistics-item text-center" style="max-width: 120px">
+                            <p>
+                                <i class="fa-regular fa-calendar-minus"></i>
+                            </p>
+                            <h2 class="" style="font-size:18px;text-transform:uppercase">Jueves</h2>
+                            <label class="badge badge-outline-success badge-pill mt-3">Ver detalles</label>
                         </div>
-                        <div class="statistics-item text-center"  style="max-width: 120px" >
-                          <p>
-                            <i class="fa-regular fa-calendar-minus"></i>
-                        </p>
-                          <h2  class="" style="font-size:18px;text-transform:uppercase">Viernes</h2>
-                          <label class="badge badge-outline-success badge-pill mt-3">Ver detalles</label>
+                        <div class="statistics-item text-center" style="max-width: 120px">
+                            <p>
+                                <i class="fa-regular fa-calendar-minus"></i>
+                            </p>
+                            <h2 class="" style="font-size:18px;text-transform:uppercase">Viernes</h2>
+                            <label class="badge badge-outline-success badge-pill mt-3">Ver detalles</label>
                         </div>
-                        <div class="statistics-item text-center"  style="max-width: 120px" >
-                          <p>
-                            <i class="fa-regular fa-calendar-minus"></i>
-                        </p>
-                          <h2  class="" style="font-size:18px;text-transform:uppercase">Sábado</h2>
-                          <label class="badge badge-outline-danger badge-pill mt-3">Ver detalles</label>
+                        <div class="statistics-item text-center" style="max-width: 120px">
+                            <p>
+                                <i class="fa-regular fa-calendar-minus"></i>
+                            </p>
+                            <h2 class="" style="font-size:18px;text-transform:uppercase">Sábado</h2>
+                            <label class="badge badge-outline-danger badge-pill mt-3">Ver detalles</label>
                         </div>
-                        <div class="statistics-item text-center"  style="max-width: 120px" >
+                        <div class="statistics-item text-center" style="max-width: 120px">
                             <p>
                                 <i class="fa-regular fa-calendar-minus"></i>
                             </p>
                             <h2 class="" style="font-size:18px;text-transform:uppercase">Domingo</h2>
                             <label class="badge badge-outline-success badge-pill mt-3">Ver detalles</label>
-                          </div>
+                        </div>
 
                     </div>
-                  </div>
                 </div>
-              </div>
+            </div>
         </div>
+    </div>
 
-
-
-    <div class="card mt-5">
-        <div class=" mb-2" style="background-color:#4b6ac3 ">
+    <div class="card mt-3" style="border-radius:10px">
+        <div class=" mb-2" style="background-color:#4b6ac3;border-radius:10px 10px 0 0 ">
             <h3 class="card-title text-center mb-4 mt-4 text-white"style="text-transform: uppercase; font-weight:bold">
                 Seguimiento del progreso</h3>
         </div>
         <div class="card-body">
-            <div class="row" >
+            <div class="row">
                 <input type="hidden" id="datosAntropometricos" value="{{ $datos }}">
                 <div class="col-md-6 grid-margin stretch-card">
                     <div class="card">
@@ -111,69 +108,142 @@
         </div>
     </div>
 
-  <div class="card  mt-5">
-    <div class="card-body">
-        <div class="d-md-flex justify-content-between align-items-center">
-          <div class="d-flex  mb-3 mb-md-0">
-            <a class="btn btn-social-icon btn-facebook btn-rounded "  href="https://www.facebook.com/Colpomed/">
-                <div class=" d-flex align-items-center  justify-content-center h-100">
-                    <i class="fab fa-facebook-f"></i>
-                </div>
-            </a>
-            <div class="ml-4">
-              <h5 class="mb-0">Facebook</h5>
-              <p class="mb-0">2887 friends</p>
-            </div>
-          </div>
-          <div class="d-flex align-items-center mb-3 mb-md-0">
-            <a class="btn btn-social-icon btn-whatsapp btn-rounded" style="background-color:#25d366;color:white "> <!-- Este faltaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa -->
-                <div class=" d-flex align-items-center  justify-content-center h-100">
-              <i class="fab fa-whatsapp"></i>
-            </div>
-
-
-            </a>
-            <div class="ml-4">
-              <h5 class="mb-0">Whatsapp</h5>
-              <p class="mb-0">Para consultas</p>
-            </div>
-          </div>
-          <div class="d-flex align-items-center mb-3 mb-md-0">
-            <a class="btn btn-social-icon btn-youtube btn-rounded" href="https://www.youtube.com/channel/UCVdJTSJolVtqvFXgwBCkOOg" style="color:white ">
-                <div class=" d-flex align-items-center  justify-content-center h-100">
-                <i class="fab fa-youtube"></i>
-                </div>
-            </a>
-            <div class="ml-4">
-              <h5 class="mb-0">Youtube</h5>
-              <p class="mb-0">Subscribete</p>
-            </div>
-          </div>
-          <div class="d-flex align-items-center mb-3 mb-md-0">
-            <a class="btn btn-social-icon btn-instagram btn-rounded" href="https://www.instagram.com/Colpomed/" style="background-color:#ac2bac;color:white ">
-            <div class=" d-flex align-items-center  justify-content-center h-100">
-              <i class="fab fa-instagram"></i>
-            </div>
-            </a>
-            <div class="ml-4">
-              <h5 class="mb-0">Instagram</h5>
-              <p class="mb-0">930 followers</p>
-            </div>
-          </div>
-          <div class="d-flex align-items-center mb-3 mb-md-0">
-            <a class="btn btn-social-icon btn-linkedin btn-rounded" href="https://www.linkedin.com/in/ximena-coronel-a82538140/" style="color:white ">
-                <div class=" d-flex align-items-center  justify-content-center h-100">
-                <i class="fab fa-linkedin-in"></i>
-                </div>
-            </a>
-            <div class="ml-4">
-              <h5 class="mb-0">Linkedin</h5>
-              <p class="mb-0">54 followers</p>
-            </div>
-          </div>
+    <div class="card mt-3" style="border-radius:10px ">
+        <div class=" mb-2" style="background-color:#4eba74;border-radius:10px 10px 0 0 " >
+            <h3 class="card-title text-center mb-3 mt-3 text-white"style="text-transform: uppercase; font-weight:bold"><i
+                    class="fas fa-dumbbell"></i>
+                Actividades<button class=" ml-3 text-right" disabled
+                    title="Aqui se encuentran actividades que tienen una alta prioridad, el resto de actividades se ecuentra en la pestaña de actividades"
+                    style="border-radius:10px; border:1px solid rgb(93, 92, 92)"><i class="fas fa-info"></i></button></h3>
         </div>
-      </div>
-  </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table text-center">
+                    <thead>
+                        <tr>
+                            <th> N°</th>
+                            <th> Imagen</th>
+                            <th> Nombre actividad </th>
+                            <th> Duración </th>
+                            <th> Prioridad </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td class="py-1">
+                                <img src="images/faces/face1.jpg" alt=" Imagen actividad" class="img-sm rounded-circle">
+                            </td>
+                            <td>
+                                Correr
+                            </td>
+                            <td>30 min</td>
+                            <td>
+                                <label class="badge badge-danger badge-pill">Alta</label>
+                            </td>
+                        </tr>
+                        <tr>
+                        <tr>
+                            <td>2</td>
+                            <td class="py-1">
+                                <img src="images/faces/face1.jpg" alt="Imagen actividad" class="img-sm rounded-circle">
+                            </td>
+                            <td>
+                                nadar
+                            </td>
+                            <td>40 min</td>
+                            <td>
+                                <label class="badge badge-danger badge-pill">Alta</label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td class="py-1">
+                                <img src="images/faces/face1.jpg" alt=" Imagen actividad" class="img-sm rounded-circle">
+                            </td>
+                            <td>
+                                Correr
+                            </td>
+                            <td>30 min</td>
+                            <td>
+                                <label class="badge badge-danger badge-pill">Alta</label>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="card  mt-3" style="border-radius:10px ">
+        <div class="card-body">
+            <div class="d-md-flex justify-content-between align-items-center">
+                <div class="d-flex align-items-center mb-3 mb-md-0">
+                    <a class="btn btn-social-icon btn-facebook btn-rounded " href="https://www.facebook.com/Colpomed/">
+                        <div class=" d-flex align-items-center  justify-content-center h-100">
+                            <i class="fab fa-facebook-f"></i>
+                        </div>
+                    </a>
+                    <div class="ml-4">
+                        <h5 class="mb-0">Facebook</h5>
+                        <p class="mb-0">2887 friends</p>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center mb-3 mb-md-0">
+                    <a class="btn btn-social-icon btn-whatsapp btn-rounded" style="background-color:#25d366;color:white ">
+                        <!-- Este faltaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa -->
+                        <div class=" d-flex align-items-center  justify-content-center h-100">
+                            <i class="fab fa-whatsapp"></i>
+                        </div>
+
+
+                    </a>
+                    <div class="ml-4">
+                        <h5 class="mb-0">Whatsapp</h5>
+                        <p class="mb-0">Para consultas</p>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center mb-3 mb-md-0">
+                    <a class="btn btn-social-icon btn-youtube btn-rounded"
+                        href="https://www.youtube.com/channel/UCVdJTSJolVtqvFXgwBCkOOg" style="color:white ">
+                        <div class=" d-flex align-items-center  justify-content-center h-100">
+                            <i class="fab fa-youtube"></i>
+                        </div>
+                    </a>
+                    <div class="ml-4">
+                        <h5 class="mb-0">Youtube</h5>
+                        <p class="mb-0">Subscribete</p>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center mb-3 mb-md-0">
+                    <a class="btn btn-social-icon btn-instagram btn-rounded" href="https://www.instagram.com/Colpomed/"
+                        style="background-color:#ac2bac;color:white ">
+                        <div class=" d-flex align-items-center  justify-content-center h-100">
+                            <i class="fab fa-instagram"></i>
+                        </div>
+                    </a>
+                    <div class="ml-4">
+                        <h5 class="mb-0">Instagram</h5>
+                        <p class="mb-0">930 followers</p>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center mb-3 mb-md-0">
+                    <a class="btn btn-social-icon btn-linkedin btn-rounded"
+                        href="https://www.linkedin.com/in/ximena-coronel-a82538140/" style="color:white ">
+                        <div class=" d-flex align-items-center  justify-content-center h-100">
+                            <i class="fab fa-linkedin-in"></i>
+                        </div>
+                    </a>
+                    <div class="ml-4">
+                        <h5 class="mb-0">Linkedin</h5>
+                        <p class="mb-0">54 followers</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
         integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>

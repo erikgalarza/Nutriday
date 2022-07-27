101672536cf958d31f617248aa953579bd62f804
AzureAD\GUSTAVOANDRESMORALES <gmorales@grupo-link.com>
2022-07-27 16:28:13 -0500

    add script para order-listing2

--- resources/views/admin/dieta/index.blade.php
+++ resources/views/admin/dieta/index.blade.php
@@ -421,7 +421,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="table-responsive">
-                        <table id="order-listing" class="table text-center">
+                        <table id="order-listing2" class="table text-center">
                             <thead>
                                 <tr>
                                     <th>N°</th>
